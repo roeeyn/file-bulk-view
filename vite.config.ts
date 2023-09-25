2c6ec5671e2ccd2753800aabf14a1ad8b6bd416f
Rodrigo Medina <r.medina@kolors.com.mx>
2023-09-25 13:11:46 -0600

    feat(hello-world): initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+// vite.config.ts
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { chromeExtension } from "vite-plugin-chrome-extension";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: "src/manifest.json"
+        }
+    },
+    plugins: [
+        chromeExtension()
+    ],
+})
